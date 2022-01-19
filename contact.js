e3d7509f3f9e425a4f3329f9763f392f08ed6d33
michael mapesa <michaelmapesa93@gmail.com>
2022-01-19 11:13:30 +0300

    add js file

---
+++ contact.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#uname").mouseover(function(){
+      $(this).css("background-color","white");
+    });
+    $("#uname").mouseout(function(){
+      $(this).css("background-color","transparent");
+    });
+    
+  });
+  
+  $(document).ready(function(){
+    $("#pwd").mouseover(function(){
+      $(this).css("background-color","white");
+    });
+    $("#pwd").mouseout(function(){
+      $(this).css("background-color","transparent");
+    });
+    
+  });
